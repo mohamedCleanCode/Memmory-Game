84c3a2858ed4af312d8f541365d141bf167a404a
mohamed <m635379@gmail.com>
2022-12-21 20:01:03 +0200

    add filter active function

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ document.querySelector(".start-game").addEventListener("click", (e) => {
   let userName = prompt("Type your name please!");
   if (userName == "" || userName == null) {
     document.querySelector(".name span").innerHTML = "Unkwon";
+    e.target.parentElement.remove();
   } else {
     userName = userName.substr(0, 1).toUpperCase() + userName.slice(1);
     document.querySelector(".name span").innerHTML = userName;
@@ -17,6 +18,28 @@ let boxs = memoryGame.querySelectorAll(".box");
 let range = [...boxs.keys()];
 let shuffledArray = range.sort((a, b) => 0.5 - Math.random());
 
+// Loop on boxs
 boxs.forEach((box, i) => {
   box.style.order = shuffledArray[i];
+  // add event click
+  box.addEventListener("click", () => {
+    // Add active
+    addActive(box);
+  });
 });
+
+// Add active function
+function addActive(selectedBox) {
+  selectedBox.classList.add("active");
+  filterActive();
+}
+
+// Filter active function
+function filterActive() {
+  let activeBox = Array.from(boxs).filter((box) =>
+    box.classList.contains("active")
+  );
+  if (activeBox.length === 2) {
+    console.log(true);
+  }
+}
